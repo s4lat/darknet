4ee96c55c61b66c097aa5268d9bbe8c36062496e
s4lat <zakazchikm@gmail.com>
2019-03-04 23:19:44 +0500

    Stupid change for google drive capability

--- src/detector.c
+++ src/detector.c
@@ -295,7 +295,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
             if (ngpus != 1) sync_nets(nets, ngpus, 0);
 #endif
             char buff[256];
-            sprintf(buff, "%s/%s_%d.weights", backup_directory, base, i);
+            sprintf(buff, "../drive/My Drive/%s/%s_%d.weights", backup_directory, base, i);
             save_weights(net, buff);
         }
         free_data(train);
