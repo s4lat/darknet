e57357034e352cbaae1c8b9feb656434aef9381a
s4lat <zakazchikm@gmail.com>
2019-03-04 20:37:24 +0500

    Change save to 50 epochs instead 1000 or 100

--- src/detector.c
+++ src/detector.c
@@ -298,16 +298,6 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
             sprintf(buff, "%s/%s_%d.weights", backup_directory, base, i);
             save_weights(net, buff);
         }
-
-        if (i >= (iter_save_last + 50)) {
-            iter_save_last = i;
-#ifdef GPU
-            if (ngpus != 1) sync_nets(nets, ngpus, 0);
-#endif
-            char buff[256];
-            sprintf(buff, "%s/%s_%d.weights", backup_directory, base, i);
-            save_weights(net, buff);
-        }
         free_data(train);
     }
 #ifdef GPU
